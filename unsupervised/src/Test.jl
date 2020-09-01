87a0f28e2b61cf416dbe73182983ad9f637ec8db
Daples <daples1@gmail.com>
2020-09-01 11:18:44 -0500

    [ uns ] added testing for mountain

--- unsupervised/src/Test.jl
+++ unsupervised/src/Test.jl
@@ -1,5 +1,6 @@
 using Distributions
 using Plots
+
 include("K-means.jl")
 include("Tools.jl")
 include("Mountain.jl")
@@ -7,16 +8,17 @@ include("Mountain.jl")
 # Create mixture
 p = 2;
 k = 2;
-n = 100;
-b = 1;
-data = rand(MvNormal(rand_params_MvN(0, b, p)...), n)'
+n = 1000;
+b = 10;
+data = rand(MvNormal(rand_params_MvN(0, b, p)...), n)';
 for j in 1:k-1
     global data = vcat(data, rand(MvNormal(rand_params_MvN(0, b, p)...), n)')
 end
 
 # Test K-Means with mixture
 # protos, U, improvs = k_means(data, k, euclidean)
-# plot_clusters(protos, U, data, "test_k_means.pdf")
+# plot_k_means(protos, U, data, "test_k_means.pdf")
 
 # Test Mountain Clusters with mixture
-protos, grid1, aux, evals = mountain_clusters(data, euclidean, 0.1, 100, 1)
+# protos, _, aux, evals = mountain_clusters(data, euclidean, 20, 1, 1, n_iter=k)
+# plot_mountains(protos, data, "test_mountain.pdf")
