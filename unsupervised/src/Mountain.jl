9f8601ce26ee43038284b32f5eaeb09120b1b2e0
Daples <daples1@gmail.com>
2020-09-02 16:38:32 -0500

    [ uns ] finished mountain clustering algorithm

--- unsupervised/src/Mountain.jl
+++ unsupervised/src/Mountain.jl
@@ -18,7 +18,10 @@ function eval_grid(grid, f)
     return f.(grid)
 end
 
-function mountain_clusters(data, dist, n_grid, σ, β; arg=nothing, n_iter=7)
+function mountain_cluster(data, dist, n_grid, σ, β;
+    n_it = 10, γ=0.4, arg=nothing, norm=true
+)
+    data = norm ? normalize(data) : data
     n, p = size(data)
     grid, aux = construct_grid(data, n_grid)
     # Iterate
@@ -26,12 +29,21 @@ function mountain_clusters(data, dist, n_grid, σ, β; arg=nothing, n_iter=7)
     fs = Vector{Function}([(v) -> sum(exp.(
         -dist(data, reshape(collect(v), (1, p)), arg).^2 / (2*σ^2)))])
     evals = []
-    for k in 1:n_iter
+    for k in 1:n_it
         push!(evals, eval_grid(grid, fs[k]))
-        protos = vcat(protos, reshape(collect(grid[argmax(evals[k])]), (1, p)))
+        ind_max = argmax(evals[k])
+        if k == 1
+            global M = evals[k][ind_max]
+        else
+            Mk = evals[k][ind_max]
+            if Mk/M < γ
+                break
+            end
+        end
+        protos = vcat(protos, reshape(collect(grid[ind_max]), (1, p)))
         push!(fs, (v) -> fs[k](v) - fs[k](protos[k+1, :])*exp(
             - dist(reshape(protos[k+1, :], (1, p)), reshape(collect(v), (1, p)),
                 arg)[1]^2 / (2*β^2)))
     end
-    return protos[2:end, :], grid, aux, evals
+    return data, protos[2:end, :], grid, aux, evals[1:end-1]
 end
