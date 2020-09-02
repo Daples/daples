a4e755a3e147aa7791d67044247f7df0adf363a2
Daples <daples1@gmail.com>
2020-09-02 16:36:43 -0500

    [ uns ] added test for subtractive algorith,

--- unsupervised/src/Test.jl
+++ unsupervised/src/Test.jl
@@ -5,11 +5,12 @@ include("K-Means.jl")
 include("Tools.jl")
 include("Mountain.jl")
 include("Fuzzy-C-Means.jl")
+include("Subtractive-Clustering.jl")
 
 # Create mixture
 p = 2;
-k = 2;
-n = 1000;
+k = 5;
+n = 2000;
 b = 20;
 data = rand(MvNormal(rand_params_MvN(0, b, p)...), n)';
 for j in 1:k-1
@@ -17,14 +18,17 @@ for j in 1:k-1
 end
 
 # Test K-Means with mixture
-# protos, U, improvs = k_means(data, k, euclidean)
-# plot_k_means(protos, U, data, "test_k_means.pdf")
+# data, protos, U, improvs = k_means(data, k, euclidean)
+# plot_k_clusters(protos, U, data, "test_k_means.pdf")
 
 # Test Mountain Clusters with mixture
-# protos, _, aux, evals = mountain_clusters(data, euclidean, 20, 1, 1, n_iter=k)
-# plot_mountains(protos, data, "test_mountain.pdf")
+data, protos, _, aux, evals = mountain_cluster(data, euclidean, 10, 0.1, 0.2)
+plot_density(protos, data, "test_mountain.pdf")
 
 # Test Fuzzy C Means
-protos, U, improvs = fuzzy_c_means(data, 2, euclidean, 2)
-fig = scatterPL(data[:, 1], data[:, 2])
-scatterPL(protos[:, 1], protos[:, 2], fig, color=:black, markersize=4)
+# data, protos, U, improvs = fuzzy_c_means(data, k, euclidean, 2)
+# plot_k_clusters(protos, U, data, "test_fuzzy_c_means.pdf")
+
+# Test Subtractive
+# data, protos, evals = subtractive_cluster(data, euclidean, 0.2)
+# plot_density(protos, data, "test_subtractive.pdf")
