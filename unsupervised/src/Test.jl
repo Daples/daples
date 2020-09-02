c012126e10b55c8bb4ce71e609859ee23b38dc3f
Daples <daples1@gmail.com>
2020-09-02 09:02:51 -0500

    [ uns ] added code to test fuzzy c means

--- unsupervised/src/Test.jl
+++ unsupervised/src/Test.jl
@@ -1,15 +1,16 @@
 using Distributions
 using Plots
 
-include("K-means.jl")
+include("K-Means.jl")
 include("Tools.jl")
 include("Mountain.jl")
+include("Fuzzy-C-Means.jl")
 
 # Create mixture
 p = 2;
 k = 2;
 n = 1000;
-b = 10;
+b = 20;
 data = rand(MvNormal(rand_params_MvN(0, b, p)...), n)';
 for j in 1:k-1
     global data = vcat(data, rand(MvNormal(rand_params_MvN(0, b, p)...), n)')
@@ -22,3 +23,8 @@ end
 # Test Mountain Clusters with mixture
 # protos, _, aux, evals = mountain_clusters(data, euclidean, 20, 1, 1, n_iter=k)
 # plot_mountains(protos, data, "test_mountain.pdf")
+
+# Test Fuzzy C Means
+protos, U, improvs = fuzzy_c_means(data, 2, euclidean, 2)
+fig = scatterPL(data[:, 1], data[:, 2])
+scatterPL(protos[:, 1], protos[:, 2], fig, color=:black, markersize=4)
