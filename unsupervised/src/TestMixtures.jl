d12d1b3d7657020508c766931e526c9055a1c06e
Daples <daples1@gmail.com>
2020-09-03 11:37:15 -0500

    [ uns ] updated code

--- unsupervised/src/TestMixtures.jl
+++ unsupervised/src/TestMixtures.jl
@@ -9,28 +9,28 @@ include("Subtractive-Clustering.jl")
 
 # Create mixture
 p = 3;
-k = 2;
-n = 250;
-b = 22;
+k = 4;
+n = 500;
+b = 50;
 data = rand(MvNormal(rand_params_MvN(0, b, p)...), n)';
-for j in 1:k-1
+for j = 1:k-1
     global data = vcat(data, rand(MvNormal(rand_params_MvN(0, b, p)...), n)')
 end
 
 # Test K-Means with mixture
-# data, protos, U, improvs = k_means(data, k, euclidean)
-# plot_k_clusters(protos, U, data, "mixture_k_means.pdf")
+data, protos, U, improvs = k_means(data, k, euclidean)
+plot_k_clusters(protos, U, data, "mixture_k_means.pdf")
 
 # Test Mountain Clusters with mixture
-# data, protos, _, aux, evals = mountain_cluster(data, euclidean, 100, 0.1, 0.2)
-# plot_density_clusters(protos, data, "mixture_mountain.pdf")
+data, protos, U, grid, aux, evals = mountain_cluster(data, euclidean, 25, 0.1, 0.2)
+plot_k_clusters(protos, U, data, "mixture_mountain.pdf")
 
 # Test Fuzzy C Means
-# data, protos, U, improvs = fuzzy_c_means(data, k, euclidean, 2)
-# plot_k_clusters(protos, U, data, "mixture_fuzzy_c_means.pdf")
+data, protos, U, improvs = fuzzy_c_means(data, k, euclidean, 2)
+plot_k_clusters(protos, U, data, "mixture_fuzzy_c_means.pdf")
 
 # Test Subtractive
-# data, protos, evals = subtractive_cluster(data, euclidean, 0.2)
-# plot_density_clusters(protos, data, "mixture_subtractive.pdf")
+data, protos, U, evals = subtractive_cluster(data, euclidean, 0.5)
+plot_k_clusters(protos, U, data, "mixture_subtractive.pdf")
 
 # Test 3D Cluster
