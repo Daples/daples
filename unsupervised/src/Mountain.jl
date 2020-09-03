2e871f6f60f3b762039db3ae0ef5d8cdc3152fd0
Daples <daples1@gmail.com>
2020-09-03 11:34:54 -0500

    [ uns ] updated code

--- unsupervised/src/Mountain.jl
+++ unsupervised/src/Mountain.jl
@@ -25,25 +25,28 @@ function mountain_cluster(data, dist, n_grid, σ, β;
     n, p = size(data)
     grid, aux = construct_grid(data, n_grid)
     # Iterate
-    protos = zeros(1, p)
     fs = Vector{Function}([(v) -> sum(exp.(
         -dist(data, reshape(collect(v), (1, p)), arg).^2 / (2*σ^2)))])
     evals = []
     for k in 1:n_it
         push!(evals, eval_grid(grid, fs[k]))
         ind_max = argmax(evals[k])
+        pₖ = reshape(collect(grid[ind_max]), (1, p))
         if k == 1
             global M = evals[k][ind_max]
+            global protos = pₖ
         else
             Mk = evals[k][ind_max]
             if abs(Mk/M) < γ
                 break
+            else
+                protos = vcat(protos, pₖ)
             end
         end
-        protos = vcat(protos, reshape(collect(grid[ind_max]), (1, p)))
-        push!(fs, (v) -> fs[k](v) - fs[k](protos[k+1, :])*exp(
-            - dist(reshape(protos[k+1, :], (1, p)), reshape(collect(v), (1, p)),
+        push!(fs, (v) -> fs[k](v) - fs[k](protos[k, :])*exp(
+            - dist(reshape(protos[k, :], (1, p)), reshape(collect(v), (1, p)),
                 arg)[1]^2 / (2*β^2)))
     end
-    return data, protos[2:end, :], grid, aux, evals[1:end-1]
+    U, _ = find_membership(data, dist, protos, arg)
+    return data, protos, U, grid, aux, evals[1:end-1]
 end
