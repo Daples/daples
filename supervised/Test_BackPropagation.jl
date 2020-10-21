afc69b42339259b988433111cba2642e30516618
Daples <daples1@gmail.com>
2020-10-21 16:36:48 -0500

    [ sup ] added prop step and started backprop.

--- supervised/Test_BackPropagation.jl
+++ supervised/Test_BackPropagation.jl
@@ -1,37 +1,73 @@
-include("Tools.jl")
+include("Tools.jl");
 
-function nn(X, Y, L, Φ, ∂Φ; η=0.05, s=10)
+function nn(X, Y, L, ϕ, ∂ϕ; η=0.05, s=10)
     # Dimensions
     N, m = size(X)
     n = size(Y, 2)
-    hidden_layers = size(L)
+
     augL = [m, L..., n]
 
+    hidden_layers = size(L, 1)
+    layers = size(augL, 1)
+
     # Auxiliary Matrices
-    Ynn = zeros(N, n)
-    E = zeros(N, s)
+    Y_nn = zeros(size(Y))
+    Ξ = zeros(N, s) # error energies per epoch
+    ∇ = zeros(N, hidden_layers + 1) # gradients per layer for each pattern
+    ∇s = []
 
     # Fill weights
     Ws = []
-    for l in 1:(size(augL) - 1)
-        append!(Ws, init_weights(L[l], L[l+1])))
+    for l in 1:(layers - 1)
+        push!(Ws, init_weights(augL[l+1], augL[l]))
     end
 
-    # for h in 1:s
-    #     for p in 1:N
-    #         x = Φ[1].(reshape(X[p, :], (m, 1)))
-    #         y = reshape(Y[p, :], (n, 1))
-    #         for i in 1:(hidden_layers + 1)
-    #             continue
-    #         end
-    #     end
-    # end
-    return Ws
+    Vs = nothing
+    E = nothing
+    ΔW = nothing
+    for h in 1:s
+        E = zeros(size(Y)) # errors in one epoch
+        for p in 1:N
+            Vs = []
+            x = ϕ[1](reshape(X[p, :], (m, 1)))
+            y = reshape(Y[p, :], (n, 1))
+
+            # Propagate
+            for i in 1:(hidden_layers + 1)
+                push!(Vs, Ws[i] * x)
+                x = ϕ[i+1](Vs[end])
+            end
+            y_nn = ϕ[end](Vs[end])
+            Y_nn[p, :] = y_nn
+            E[p, :] = y - y_nn
+
+            # Backpropagate
+            aux = true
+            for i in (hidden_layers + 1):-1:1
+                e = reshape(E[p, :], (n, 1))
+                if aux
+                    δ = e .* ∂ϕ[i+1](Vs[i])
+                    aux = false
+                else
+                    ##### δ = ∂ϕ[i+1](Vs[i])
+                end
+                ∇[p, :] = δ
+                ΔW = -η * δ * Vs[i-1]'
+                Ws[i] += ΔW
+            end
+        end
+        push!(∇s, ∇)
+    end
+    return ΔW
 end
 
-# Propagate pattern x (mx1) and associated output y (nx1)
-function prop(x, y, ϕ, ∂ϕ; η=0.1, s=10)
-    continue
-end
+# XOR
+X = [0 0; 0 1; 1 0; 1 1];
+Y = [0; 1; 1; 0];
+L = [3, 4, 2];
+ϕ_sigm(x) = 1.0 ./ (1.0 .+ exp.(-x));
+∂ϕ_sigm(x) = ϕ_sigm(x).*(1 .- ϕ_sigm(x));
 
-nn()
+ϕ = [ϕ_sigm for i in 1:size(L, 1)+2]
+∂ϕ = [∂ϕ_sigm for i in 1:size(L, 1)+2]
+a = nn(X, Y, L, ϕ, ∂ϕ)
