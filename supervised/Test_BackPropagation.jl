7f6f18fc9e096fd46e4d53c0a49205649ee79e42
Daples <daples1@gmail.com>
2020-10-22 18:12:00 -0500

    [ sup ] initial backprop algorithm

--- supervised/Test_BackPropagation.jl
+++ supervised/Test_BackPropagation.jl
@@ -1,6 +1,6 @@
 include("Tools.jl");
 
-function nn(X, Y, L, ϕ, ∂ϕ; η=0.05, s=10)
+function nn(X, Y, L, ϕ, ∂ϕ; η = 0.05, s = 10, seed = nothing)
     # Dimensions
     N, m = size(X)
     n = size(Y, 2)
@@ -13,29 +13,33 @@ function nn(X, Y, L, ϕ, ∂ϕ; η=0.05, s=10)
     # Auxiliary Matrices
     Y_nn = zeros(size(Y))
     Ξ = zeros(N, s) # error energies per epoch
-    ∇ = zeros(N, hidden_layers + 1) # gradients per layer for each pattern
+    ∇ = zeros(N, hidden_layers + 1) # sum of gradients per layer for each pattern
     ∇s = []
 
     # Fill weights
     Ws = []
     for l in 1:(layers - 1)
-        push!(Ws, init_weights(augL[l+1], augL[l]))
+        push!(Ws, init_weights(augL[l+1], augL[l], seed=seed))
     end
 
     Vs = nothing
     E = nothing
     ΔW = nothing
+    δs = nothing
     for h in 1:s
         E = zeros(size(Y)) # errors in one epoch
         for p in 1:N
             Vs = []
-            x = ϕ[1](reshape(X[p, :], (m, 1)))
+            δs = []
+            x = reshape(X[p, :], (m, 1))
+            push!(Vs, ϕ[1](x))
             y = reshape(Y[p, :], (n, 1))
 
             # Propagate
+            aux = Vs[end]
             for i in 1:(hidden_layers + 1)
-                push!(Vs, Ws[i] * x)
-                x = ϕ[i+1](Vs[end])
+                push!(Vs, Ws[i] * aux)
+                aux = ϕ[i+1](Vs[end])
             end
             y_nn = ϕ[end](Vs[end])
             Y_nn[p, :] = y_nn
@@ -43,19 +47,21 @@ function nn(X, Y, L, ϕ, ∂ϕ; η=0.05, s=10)
 
             # Backpropagate
             aux = true
-            for i in (hidden_layers + 1):-1:1
+            for i in layers:-1:2
                 e = reshape(E[p, :], (n, 1))
                 if aux
-                    δ = e .* ∂ϕ[i+1](Vs[i])
+                    δ = e .* ∂ϕ[i](Vs[i])
                     aux = false
                 else
-                    ##### δ = ∂ϕ[i+1](Vs[i])
+                    δ = ∂ϕ[i](Vs[i]) .* (Ws[i]' * δs[1])
                 end
-                ∇[p, :] = δ
+                pushfirst!(δs, δ)
+                ∇[p, i-1] = sum(δ)
                 ΔW = -η * δ * Vs[i-1]'
-                Ws[i] += ΔW
+                Ws[i-1] += ΔW
             end
         end
+        Ξ[:, h] = sum(0.5 * E.^2, dims=2)
         push!(∇s, ∇)
     end
     return ΔW
