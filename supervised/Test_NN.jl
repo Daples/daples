68ddd8618213570cd2022253dfee427d0dde73e0
Daples <daples1@gmail.com>
2020-10-27 08:28:02 -0500

    [ sup ] testing

--- supervised/Test_NN.jl
+++ supervised/Test_NN.jl
@@ -16,27 +16,27 @@ L = [3, 2];
 ϕ_tanh(x) = tanh.(x)
 ∂ϕ_tanh(x) = 1 .- (ϕ_tanh(x)).^2
 
-ϕ = [ϕ_sigm for i in 1:size(L, 1)+2]
-∂ϕ = [∂ϕ_sigm for i in 1:size(L, 1)+2]
+ϕ = [ϕ_sigm for i in 1:size(L, 1) + 2]
+∂ϕ = [∂ϕ_sigm for i in 1:size(L, 1) + 2]
 
 # NN
-s = 50
-η = 0.75
+s = 20
+η = 0.9
 α = 0
 Vs, Φs, Ws, ∇s, Ξ = nn(X, Y, L, ϕ, ∂ϕ, s=s, η=η, α=α)
 
 # Plot Average Error
-plot_ξav(Ξ)
+# plot_ξav(Ξ)
 
 # Plot Gradient
 plot_∇s(∇s)
 
 # Plot gradient's evolution in layer l for different seeds
-S = rand(1:1000, 10)
-l = 1
-plot_seed∇(l, S, X, Y, L, ϕ, ∂ϕ, s=s, η=η, α=α)
+# S = rand(1:1000, 10)
+# l = 1
+# plot_seed∇(l, S, X, Y, L, ϕ, ∂ϕ, s=s, η=η, α=α)
 
 # NN output
-# for i in 1:size(X, 1)
-#     print(propagate(X[i, :], Ws, ϕ, size(L, 1))[2][end])
-# end
+for i in 1:size(X, 1)
+    print(propagate(X[i, :], Ws, ϕ, size(L, 1))[2][end])
+end
