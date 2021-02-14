234df30d8bfb427cb757643254dcbf0552c5bc01
daples <daples1@gmail.com>
2021-02-14 12:17:01 +0100

    [ hybrid ] deleted folder

--- hybrid/src/Tools.jl
+++
@@ -1,6 +0,0 @@
-using MLDatasets
-using Plots
-
-function plot_number(mat)
-    return heatmap(mat, color = :greys)
-end
--- hybrid/src/leNet5.jl
+++
@@ -1,5 +0,0 @@
-include("Tools.jl")
-
-
-train_x, train_y = MNIST.traindata();
-test_x, test_y = MNIST.testdata();
